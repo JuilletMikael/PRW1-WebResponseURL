31d27d3fa8aca2b357d3bae8e8084a6b43789654
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-09-02 17:56:57 +0200

    new method started

--- index.php
+++ index.php
@@ -1,30 +1,13 @@
 <?php
 require_once "controller/controller.php";
-require_once "controller/cities.php";
 
 if(isset($_SERVER['REQUEST_URI'])){
-    $action = explode("/", $_SERVER['REQUEST_URI']);
-    $mims = $_SERVER['HTTP_ACCEPT'];
+    $request = $_SERVER['REQUEST_URI'];
+    $mims = explode(",", $_SERVER['HTTP_ACCEPT']);
 
-    switch($action[1]) {
-        case 'people':
-            if ($action[2])
-            {
-                controller($action[1], $action[2], $mims);
-            }
-            else{
-                //TODO : need to check if ther's the right answer that can be given
-                peopleList();
-            }
-            break;
-        case 'cities':
-            if ($action[2])
-            {
-                cityController($action[2], $mims);
-            }
-            else{
-                citiesList();
-            }
-            break;
-    }
+    match($request) {
+        '/people' => peopleController($request, $mims),
+        '/cities' => citiesController($request, $mims),
+        '/' => require_once __DIR__ . "/view/home.php"
+    };
 }
